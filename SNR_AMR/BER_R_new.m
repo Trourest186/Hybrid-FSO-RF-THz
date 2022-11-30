28aeb190ec99b19c129a6cb74633cf9b2183837f
Trourest186 <74035725+Trourest186@users.noreply.github.com>
2022-11-30 20:04:32 +0700

    Delete SNR_AMR directory

--- SNR_AMR/BER_R_new.m
+++
@@ -1,5 +0,0 @@
-function result = BER_R_new(N,x)
-% x : SNR_FSO
-result = ((N-1)*(erfc(sqrt((3*x)/(2*(N-1)*(2*N-1))))))/(N*log2(N)) ;
-end
-
--- SNR_AMR/SNR_FSO_AMR1.m
+++
@@ -1,8 +0,0 @@
-function FSO  = SNR_FSO_AMR1(j)
-BER_0 = 10^-5;
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-tmp = erfc(((2^j)*j*BER_0)/((2^j)-1));
-FSO = (2*((2^j)-1)*((2^(j+1))-1))/(3*tmp^2);
-end
-
